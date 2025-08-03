bb7d4fde86ab2c828fb174f8b700c8f17de53335
CrSimple-d <crsimple0@gmail.com>
2025-08-03 11:35:38 +0300

    drops upd

--- src/main/java/net/flaulox/create_currency_shops/common/registries/CreateCurrencyShopsBlocks.java
+++ src/main/java/net/flaulox/create_currency_shops/common/registries/CreateCurrencyShopsBlocks.java
@@ -3,15 +3,21 @@ package net.flaulox.create_currency_shops.common.registries;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.flaulox.create_currency_shops.CreateCurrencyShops;
 import net.flaulox.create_currency_shops.common.blocks.Exchanger;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
 
 public class CreateCurrencyShopsBlocks {
     private static final CreateCurrencyShopsRegistrate REGISTRATE = CreateCurrencyShops.registrate();
 
     public static final BlockEntry<Exchanger> EXCHANGER = REGISTRATE.block("exchanger", Exchanger::new)
-            .properties(BlockBehaviour.Properties::noOcclusion)
+            .properties(p -> p.requiresCorrectToolForDrops()
+                    .mapColor(MapColor.WOOD)
+                    .strength(2f,3f)
+                    .sound(SoundType.WOOD)
+                    .noOcclusion())
             .blockstate((ctx,prov) -> prov.horizontalBlock(ctx.getEntry(),prov.models().getExistingFile(ctx.getId())))
             .item((e,p) -> {
                 p.rarity(Rarity.UNCOMMON);
